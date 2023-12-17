8c78f5e742c157b8f1f8d814edcaaf5804f98bad
DESKTOP-BJU5KBH\Kyle <kweb@rogers.com>
2023-12-17 14:28:38 -0500

    first commit

---
+++ UDPClientGFX/gameplay/playercontrollercomponent.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <common.h>
+
+#include <glm/glm.hpp>
+
+#include <system/component.h>
+
+gdpNamespaceBegin
+struct PlayerControllerComponent : public Component
+{
+public:
+	PlayerControllerComponent() { }
+
+	bool moveForward = false;
+	bool moveBackward = false;
+	bool moveLeft = false;
+	bool moveRight = false;
+	bool shoot = false;
+};
+gdpNamespaceEnd
